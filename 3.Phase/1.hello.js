cc199cde0b15518913d4f83c092a086cf6f1ad02
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-07 15:00:22 +0530

    hello

---
+++ 3.Phase/1.hello.js
@@ -0,0 +1,16 @@
+function printchai() {
+    console.log("Hello chai")
+}
+
+printchai();
+
+
+
+
+//  dir = ek command(linux k seekh lo) h jo windwos base h. (ise kya hota files dikhti h, folder me de rakha h)
+// 1. function        = function mtlb ek factory h.
+// 2. printpriya      = uske bad factory ka name v to likhna hota h, 
+// 3. ()              = india ke andr jis trh ek factory chalne ke liye licences lete h, same usi trh js ke andr (syntax) follow krna hoga.
+// 4. {}              = ab ise curely barces ke andr ap jo likhoge n oo apka kam h.
+// 5. Print           = print jab v karwana ho to uske liye:-  console.log("hello priya");
+// 6. Printchai       = 
