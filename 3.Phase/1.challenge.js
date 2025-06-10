b0048a59d664c8c04bcf263efcf080c2155b7343
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-10 23:21:11 +0530

    H

---
+++ 3.Phase/1.challenge.js
@@ -0,0 +1,9 @@
+// you and your friend are baking cupcakes  for a partya . you've filled up two trays, each with a certain number of cupcakes. you want to know the total number of cupcakes you have before you frost them.
+
+// CHALLENGE :- Create a function that adds two numbers (representing cupckes on each tray) and returns the total.
+
+
+function totalcupcakes(trayOne, trayTwo) {
+    // return the sum of trayOne and trayTwo
+
+}
