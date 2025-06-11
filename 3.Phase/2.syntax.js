8a3e55a52acc287c24d8b9ae77424e59a4b52815
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-11 23:40:05 +0530

    S

---
+++ 3.Phase/2.syntax.js
@@ -0,0 +1,28 @@
+// factory k syntax start hota h:-  function.
+// then factory name :- printchai
+// then parameters () then, {} :- curely barces ke andr jo v kch likhnege oo apka km h,
+// to kya km karna h, to uske liye ; console :- kr lete h. 
+// then factory ko call v to karwana hoga, to uske liye hr factory ko "uske nam se bulate" h, ni to km ni krega agr uske nam se ni bulange to.
+// uske bd paranthesis lga denge ();
+
+
+function printchai() {
+    console.log("hello chai");
+}
+
+function bringbrush(item) {
+    console.log(`hanji, le aye ${item} brush hmere liye`);
+}
+
+function addTwo(ekNum, doNum) {
+    return ekNum + doNum;
+}
+
+
+bringbrush(4);
+printchai();
+addTwo(1, 1);
+console.log(addTwo);
+
+
+
