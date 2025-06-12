ee80f55dc57774fbb68f2f8563d8fe9368346818
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-12 22:55:25 +0530

    c

---
+++ 3.Phase/2.challenge.js
@@ -0,0 +1,11 @@
+// Create a function that "multiplies" two numbers (chocolate bars per box and number of boxes) and returns the total number of chocolate bars needed.
+
+
+
+// you just need to implement the totalchocolateBars function
+function totalchocolateBars(barsperbox, numOfBoxes) {
+    // return barsperbox multiplied by numberofboxes
+
+}
+
+totalchocolateBars 
