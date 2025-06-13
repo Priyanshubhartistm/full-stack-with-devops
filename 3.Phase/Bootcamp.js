c6893dd0d07abb5c7641cdddc0c880b62e115839
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-13 22:34:46 +0530

    b

---
+++ 3.Phase/Bootcamp.js
@@ -0,0 +1,38 @@
+//  DATATYPES are 2 type :- 
+
+// ** primitive data type
+
+// 1. String :-            "" , '', ``(backtics)
+let name = "priya"
+
+// 2. Number :-            45 , 45.5
+let age = 21
+
+// 3. Boolean :-           true, false
+let ispaid = true
+
+// 4. null :-               empty(janbhujkr)
+let favouriteClass = null
+console.log(favouriteClass);      // null
+
+
+// 5. undefined :-             Memory me h, but; baad me dekhenge
+let hometown;
+console.log(hometown);            // undefined
+
+//  ** Non primitive data type:- y dono alg alg data type h.
+
+//  6. Array
+let skills = ["html", "css", "js"];
+
+// 7. Object
+studentProfile = {
+    name: "priya",
+    age: 21,
+    ispaid: true,
+    skills: ["html", "css", "js"],
+    favouriteClass: null
+};
+
+// kise v chiz k data type pata krn ho to
+console.log(typeof hometown);
