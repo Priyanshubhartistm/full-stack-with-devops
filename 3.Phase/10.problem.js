3ca46bf1be069ef75cb3af4051b620a95420cad3
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-06-29 08:21:29 +0530

    problem

---
+++ 3.Phase/10.problem.js
@@ -0,0 +1,24 @@
+// 1 Problem: Create an array containing different types of teas.
+
+const teas = ["green tea", "black tea", "oolong tea", "white tea", "herbal tea"]
+console.log(teas);
+
+// 2 Problem: Add "chamolie tea" to the exisiting list of teas.
+
+teas.push("chamomile tea");
+// 3 Problem: Remove "OOLONG tea" from the list of teas.
+
+// 4 Problem: Filtter the list to only include teas that are caffeinated.
+
+// 5 Problem: Sort the list of teas in alphabetic order.
+
+// 6 Problem: Use a for loop to print each types of tea in the array.
+
+// 7 Problem: Use a for loop to count how many teas are caffeinated (excluding "herbal tea").
+
+// 8 Problem: Use a for loop to create a new array with all tea names in upercase.
+
+// 9 Problem: Use a for loop to find the tea name with the most characters.
+
+// 10 Problem: Use a for loop to reverse the order of teeas in the array.
+
