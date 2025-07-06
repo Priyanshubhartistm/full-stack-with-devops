3d35da62e3e6cc8aec1f24f7fdadc3233a1e7757
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-06 08:39:14 +0530

    object

---
+++ 3.Phase/11.obj.js
@@ -0,0 +1,9 @@
+// objects - Data structures in JS
+//  that store data 
+
+// two types of data structures in JS
+// 1.primitives        = outof the box
+//  - string, number, boolean, null, undefined, symbol, bigint
+
+// 2.non-primitives    = reference types
+//  - objects, arrays, functions
