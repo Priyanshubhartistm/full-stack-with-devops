e1ce12dc7bb8b6541441b17ae8484aae3e5afced
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-07 12:33:27 +0530

    quokka

---
+++ 3.Phase/12.quokka.js
@@ -0,0 +1,13 @@
+// const x = 10
+
+// console.log(x);
+
+const person = {
+    name: "John",
+    age: 30
+},
+address = {
+    city: "New York",
+    country: "USA"
+};
+console.log(person.name); // Accessing property using dot notation
