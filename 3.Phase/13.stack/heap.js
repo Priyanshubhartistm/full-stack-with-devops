1ea262260f9a7b31c427e8b13db05b45e7a17c4c
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-07 12:36:40 +0530

    heap

---
+++ 3.Phase/13.stack/heap.js
@@ -0,0 +1,15 @@
+// VARIABLE kya h?    = ek memory (kise v language ke andr n hmre pass, 2trah ki memory hoti h,  )
+
+//  Memory has 2 types 
+// STACK                 = shadi me jo plates hoti h, jo akhir me hogi, oo sbse phle uthyenge.
+// stack ke andr kya hota h, n apke jitne v variables hote h, n oo sb ise "stack memory" ko use krte h.
+
+// const fname = 'priya'
+// const lname = 'bharti'         ; jb ise particular lname k scope khtm ho jayega to, ya yaha se popout(hat) jayega,
+
+
+// ** note:- jb sara kam stack se ho ja rha h, to heap ki kya jrurt h, yaha par stack ki ek problem stack "grow" ni kar skti h,
+// kya array grow kr skta h, agr aj 10 "element" h, to kl 3 ho skte h,
+// kya object ke andr 4 keys h, to 5 keys ho skti h, dynamically growable chize hoti h, waha pr stack km ni krta h,
+
+// HEAP                   = 
