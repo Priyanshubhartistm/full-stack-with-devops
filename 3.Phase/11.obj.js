8abb4bfcd3345a5aa53e8312cf2263a8bb3c53bc
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-08 17:24:33 +0530

    obj

--- 3.Phase/11.obj.js
+++ 3.Phase/11.obj.js
@@ -2,7 +2,7 @@
 //  that store data 
 
 // two types of data structures in JS
-// 1.primitives        = outof the box
+// 1.primitives        = outof the box support krte h. meand array jo h, oo inbuilt h, already
 //  - string, number, boolean, null, undefined, symbol, bigint
 
 // 2.non-primitives    = reference types
