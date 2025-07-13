dde8cac52c95df24a514889a3a89b12203d6d8e5
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-13 22:45:00 +0530

    function

---
+++ 3.Phase/15.function.js
@@ -0,0 +1,12 @@
+Object.prototype.chai = function() {
+    console.log("chai aur code");
+    
+}
+const tea = {
+    name: "Masala Chai",
+    type: "Spiced",
+
+};
+tea.chai()
+const myteas = ["lemon tea", "yellow tea"]
+    myteas.chai()
