641cabb4ec3da0aad218fa8a152a285ed2b1260e
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-14 17:17:11 +0530

    prototype

---
+++ 3.Phase/16.prototype.js
@@ -0,0 +1,25 @@
+// Prototype.js  means ki jb ham ek object bante h, to uske andr kch properties aur methods hote h, jo ki us object ke prototype se aate h.
+
+const arr = [1,2,3]
+arr 
+
+const str = 'Priya'
+str.search      // str ke bad hm jaise hi dot (.) lgate h, 
+
+const obj = {x: 1 , length: 1}
+obj.x
+
+//  Dot operator "object" ke properties ko access krne me help karta hai
+
+// Prototype  = jb ham ek ARRAY bante h, to uske andr kch function bnate h, (.map, .filter, .reduce) jo ki bs array ke andr banate h.
+
+const arr1 = [1,2,3]
+//  jab v hm ek array bante h, tb uske andr y sb upr wale function aa jate h,
+
+arr 
+// y arr kis type k data type h, to h array type ka data type
+
+
+// NOTE:- jab v koye v js ke andr kch v array bante h, to uske andr y upr wale function aa jate h, jo ki array ke prototype se aate h.
+//  Prototype ka matlab h ki jb v kch v object bante h, to uske andr kch properties aur methods hote h, jo ki us object ke prototype se aate h.
+
