f93862d1b9f7b91784e51395cfda167453d226be
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-20 19:05:01 +0530

    dom

---
+++ 4.Phase/1.dom.js
@@ -0,0 +1,34 @@
+/*
+1. DOM = Document Object Model
+
+dom are basically html elements that are used to create the structure of a web page.
+
+js ne hmre html ko badla,iske click pr js y fir kch to chiz h, background,me jo hmre html ko dynamically change kar rhi h.
+
+agar nhtml k structure change ho raha h, to y sb hamre js kar rhi h, to js ke andr ek concept hota h, jise khte h, "dom manipulation".
+
+
+2. DOM Manipulation = Changing the structure of a web page using JavaScript
+
+js ko use karke aap "DOM" ko manipulate kar sakte ho, yani ki aap HTML elements ko add, remove, ya modify kar sakte ho.
+
+3. Example of DOM Manipulation
+Example: Adding a new paragraph to a web page
+```javascript
+
+Select the body element
+const body = document.querySelector('body');
+
+Create a new paragraph element
+const newParagraph = document.createElement('p');
+
+NOTE:- Terminal spinup krn h
+
+JS ko do jhugh chal skte h
+1. Browser Console
+2. Node.js Environment
+
+
+
+
+*/
