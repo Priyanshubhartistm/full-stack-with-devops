25a2290d3dd77f5e9009fd4189cd52312cb73335
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-07-24 23:48:23 +0530

    index

---
+++ 4.Phase/4.index.js
@@ -0,0 +1,6 @@
+// kya window hamri js k part h, ni agar hota to , node me v chalta, to y kn kr rha h, y browser, y browser ise object ko define krta h, '
+// browser ise object ko define krta h, so that is why y particular code hamre browser me to sahi chal raha h, but hmre y node me ni chal raha h, to(node js)ko use ni krenge
+
+console.log(window);
+
+window.document.write("Hello Priyanshu js");
---
+++ 4.Phase/5.manupulation.js
@@ -0,0 +1,22 @@
+/*
+DOM Manupulation have only two story
+1. kab krna h                    = agr button click hota h, to bg color black kr do.
+2. or kya karna h                = user k wait krenge button click krne k , o jaise hi krega button click, then hm 2nd task krenge
+
+
+
+
+
+
+*/
+
+
+function changeBackgroundBlack() {
+    document.body.style.backgroundColor = "black";
+}
+
+changeBackgroundBlack();
+
+function changeBackgroundBlack() {
+    document.body.style.backgroundColor = "black";
+}
